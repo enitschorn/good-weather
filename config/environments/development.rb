7c7f9dd9bd8d682335883e45abc58342bbaf1fe8
Michael Milewski <saramic@gmail.com>
2021-01-27 22:41:21 +1100

    :wrench: letter opener config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  Rails.application.routes.default_url_options = {
+    host: "localhost",
+    port: "3000",
+    protocol: "http",
+  }
+  config.default_from_email = "from@example.com"
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
