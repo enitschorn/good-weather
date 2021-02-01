fbcf1609ee9dbf6ffae4086d1f73eaace61fe78a
Christine Schorn <cschorn91@gmail.com>
2021-02-01 22:34:34 +1100

    seed db with cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,69 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+LOCATIONS = [
+  { :city => "Melbourne", :latitude => -37.813629, :longitude => 144.963058, :is_fetching_forecast => true},
+  { :city => "Geelong", :latitude => -38.149918, :longitude => 144.361725, :is_fetching_forecast => true},
+  { :city => "Ballarat", :latitude => -37.560900, :longitude => 143.854970, :is_fetching_forecast => true},
+  { :city => "Bendigo", :latitude => -36.757015, :longitude => 144.212285, :is_fetching_forecast => true},
+  { :city => "Melton", :latitude => -37.677725, :longitude => 144.5288164, :is_fetching_forecast => true},
+  { :city => "Mildura", :latitude => -34.2100986, :longitude => 142.0975099, :is_fetching_forecast => true},
+  { :city => "Shepparton", :latitude => -36.3733897, :longitude => 145.3569005, :is_fetching_forecast => true},
+  { :city => "Mooroopna", :latitude => -36.3986656, :longitude => 145.3132556, :is_fetching_forecast => true},
+  { :city => "Pakenham", :latitude => -38.0710532, :longitude => 145.4527787, :is_fetching_forecast => true},
+  { :city => "Wodonga",  :latitude => -36.1166378, :longitude => 146.854262, :is_fetching_forecast => true},
+  { :city => "Albury",  :latitude => -36.0653583, :longitude => 146.8061647, :is_fetching_forecast => true},
+  { :city => "Sunbury",  :latitude => -37.5533712, :longitude => 144.6364504, :is_fetching_forecast => true},
+  { :city => "Warrnambool",  :latitude => -38.378649, :longitude => 142.4416859, :is_fetching_forecast => true},
+  { :city => "Traralgon",  :latitude => -38.2017841, :longitude => 146.4409639, :is_fetching_forecast => true},
+  { :city => "Wangaratta",  :latitude => -36.3542735, :longitude => 146.2712661, :is_fetching_forecast => true},
+  { :city => "Ocean Grove",  :latitude => -38.2602662, :longitude => 144.5030703, :is_fetching_forecast => true},
+  { :city => "Bacchus Marsh",  :latitude => -37.6690109, :longitude => 144.3904632, :is_fetching_forecast => true},
+  { :city => "Torquay",  :latitude => -38.3128041, :longitude => 144.2595976, :is_fetching_forecast => true},
+  { :city => "Horsham",  :latitude => -36.7379039, :longitude => 142.1538197, :is_fetching_forecast => true},
+  { :city => "Moe",  :latitude => -38.1749814, :longitude => 146.2144622, :is_fetching_forecast => true},
+  { :city => "Warragul",  :latitude => -38.1491719, :longitude => 145.8571315, :is_fetching_forecast => true},
+  { :city => "Morwell",  :latitude => -38.2315775, :longitude => 146.3499886, :is_fetching_forecast => true},
+  { :city => "Sale",  :latitude => -38.1059469, :longitude => 147.0006442, :is_fetching_forecast => true},
+  { :city => "Lara",  :latitude => -38.0104709, :longitude => 144.3439876, :is_fetching_forecast => true},
+  { :city => "Bairnsdale",  :latitude => -37.8227542, :longitude => 147.5893172, :is_fetching_forecast => true},
+  { :city => "Echuca",  :latitude => -36.1547766, :longitude => 144.6917538, :is_fetching_forecast => true},
+  { :city => "Drysdale",  :latitude => -38.1749477, :longitude => 144.5280319, :is_fetching_forecast => true},
+  { :city => "Colac",  :latitude => -38.3529649, :longitude => 143.603696, :is_fetching_forecast => true},
+  { :city => "Drouin",  :latitude => -38.1428508, :longitude => 145.7703427, :is_fetching_forecast => true},
+  { :city => "Leopold",  :latitude => -38.1963562, :longitude => 144.427532, :is_fetching_forecast => true},
+  { :city => "Swan Hill",  :latitude => -38.3567408, :longitude => 143.4533886, :is_fetching_forecast => true},
+  { :city => "Portland",  :latitude => -38.0710532, :longitude => 141.5744996, :is_fetching_forecast => true},
+  { :city => "Castlemaine",  :latitude => -37.070717, :longitude => 144.1494673, :is_fetching_forecast => true},
+  { :city => "Gisborne",  :latitude => -37.4757395, :longitude => 144.5556384, :is_fetching_forecast => true},
+  { :city => "Benalla",  :latitude => -36.5707513, :longitude => 145.8667222, :is_fetching_forecast => true},
+  { :city => "Hamilton",  :latitude => -37.7414255, :longitude => 141.944722, :is_fetching_forecast => true},
+  { :city => "Wallan",  :latitude => -37.4068538, :longitude => 144.8909556, :is_fetching_forecast => true},
+  { :city => "Healesville",  :latitude => -37.6569111, :longitude => 145.484684, :is_fetching_forecast => true},
+  { :city => "Wonthaggi",  :latitude => -38.6023453, :longitude => 145.5710323, :is_fetching_forecast => true},
+  { :city => "Yarrawonga",  :latitude => -36.0307369, :longitude => 145.9379241, :is_fetching_forecast => true},
+  { :city => "Maryborough",  :latitude => -37.0499513, :longitude => 143.6697776, :is_fetching_forecast => true},
+  { :city => "Kilmore",  :latitude => -37.2983434, :longitude => 144.888964, :is_fetching_forecast => true},
+  { :city => "Kyneton",  :latitude => -37.2231476, :longitude => 144.3777627, :is_fetching_forecast => true},
+  { :city => "Ararat",  :latitude => -37.2655227, :longitude => 142.8396505, :is_fetching_forecast => true},
+  { :city => "Portarlington",  :latitude => -38.1396967, :longitude => 144.6215225, :is_fetching_forecast => true},
+  { :city => "Lakes Entrance",  :latitude => -37.8693828, :longitude => 147.9223688, :is_fetching_forecast => true},
+  { :city => "Kyabram",  :latitude => -36.326504, :longitude => 144.8918655, :is_fetching_forecast => true},
+  { :city => "Seymour",  :latitude => -37.0309662, :longitude => 145.0912546, :is_fetching_forecast => true},
+  { :city => "Stawell",  :latitude => -37.0444133, :longitude => 142.6884806, :is_fetching_forecast => true},
+  { :city => "Cobram",  :latitude => -35.9232374, :longitude => 145.5686097, :is_fetching_forecast => true},
+  { :city => "Irymple",  :latitude => -34.2456471, :longitude => 142.0974652, :is_fetching_forecast => true},
+  { :city => "Leongatha",  :latitude => -38.4718878, :longitude => 145.8717146, :is_fetching_forecast => true},
+  { :city => "Inverloch",  :latitude => -38.6201407, :longitude => 145.5940438, :is_fetching_forecast => true},
+  { :city => "Red Cliffs",  :latitude => -34.3386922, :longitude => 142.1119053, :is_fetching_forecast => true},
+  { :city => "Bannockburn",  :latitude => -38.0488868, :longitude => 144.0872314, :is_fetching_forecast => true},
+  { :city => "Cowes",  :latitude => -38.4545785, :longitude => 145.1781154, :is_fetching_forecast => true}
+]
+
+if Rails.env.development?
+  puts 'Cleaning up database'
+  Location.destroy_all
+end
+
+LOCATIONS.each do |location|
+  Location.create!(name: (location[:city]), latitude: (location[:latitude]), longitude: (location[:longitude]), is_fetching_forecast: (location[:is_fetching_forecast]))
+end
+
+puts 'Created locations'
