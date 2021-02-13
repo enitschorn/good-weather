7bea27cdc80dc9d622345ff50c2311fed63d8558
Michael Milewski <saramic@gmail.com>
2021-02-14 08:34:01 +1100

    cut & paste error around field names

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -2,10 +2,10 @@ class Forecast < ApplicationRecord
   belongs_to :location_forecast
 
   def daily_data=(value)
-    self[:user_actions] = value.is_a?(String) ? JSON.parse(value) : value
+    self[:daily_data] = value.is_a?(String) ? JSON.parse(value) : value
   end
 
   def hourly_data=(value)
-    self[:user_actions] = value.is_a?(String) ? JSON.parse(value) : value
+    self[:hourly_data] = value.is_a?(String) ? JSON.parse(value) : value
   end
 end
